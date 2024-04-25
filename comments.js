47e5dc083e76431ebecce3f6ee94e14f2fa289d5
Kaow <wukevin94000@gmail.com>
2024-04-25 08:44:48 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,5 +1,18 @@
-//Create web server
+// Importation du module Express
 const express = require('express');
+
+// Création d'une nouvelle application Express
 const app = express();
+
+// Définition du port
 const port = 3000;
 
+// Configuration de la route d'accueil
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Démarrage du serveur
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});

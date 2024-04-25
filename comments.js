64d44eb89fc7c4e563ecdda9dc06e60f56c500d5
Kaow <wukevin94000@gmail.com>
2024-04-25 08:53:01 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,12 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
 });
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
